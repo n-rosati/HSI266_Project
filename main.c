768713108dae918330f5afb9620efbb6c470417c
Nicholas Rosati <nicholas@rosati.xyz>
2025-03-08 00:24:29 -0500

    starter labjack code

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include "C:/Program Files (x86)/LabJack/Drivers/LabJackUD.h"
 
 int main(void) {
-    printf("Hello, World!\n");
+    LJ_HANDLE lj_handle = 0;
+    double ljSN = 0;
+
+    OpenLabJack(LJ_dtU3, LJ_ctUSB, "1", 1, &lj_handle);
+    ePut(lj_handle, LJ_ioPIN_CONFIGURATION_RESET, 0, 0, 0);
+
+    eGet(lj_handle, LJ_ioGET_CONFIG, LJ_chSERIAL_NUMBER, &ljSN, 0);
+    printf("Serial number: %.0lf\n", ljSN);
+
     return 0;
 }
