240a2c12de44c28f00a851ad692945bcb9cd3083
Nicholas Rosati <nicholas@rosati.xyz>
2025-03-09 18:27:07 -0400

    add a constants header

---
+++ include/constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define PIN_SERVO   8
+#define PIN_A       17
+#define PIN_B       18
+#define PIN_C       12
+#define PIN_D       14
+#define PIN_BTN     13
+#define PIN_RB_SENS 15
+
+#define THREAD_SLEEP_MS 50
+
+#endif //CONSTANTS_H
